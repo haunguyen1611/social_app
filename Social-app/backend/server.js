04f5a852d51c853e774a5169031bc6f25345acf1
haunguyen1611 <150682278+haunguyen1611@users.noreply.github.com>
2024-12-26 09:27:20 +0700

    Update server.js

--- Social-app/backend/server.js
+++ Social-app/backend/server.js
@@ -25,7 +25,7 @@ cloudinary.config({
 
 app.use(cors(
     {
-        origin: "http://localhost:5000",
+        origin: "https://social-app-seven-ashen.vercel.app",
         methods: ["GET","POST","PUT","DELETE"],
         credentials: true
     }
