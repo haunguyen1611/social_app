d7f3b7ce30a3d8156ecf0ac079616bf1b2f2a3d6
hau1611 <haunn.22ite@vku.udn.vn>
2024-12-26 18:52:19 +0700

    update

--- Social-app/backend/server.js
+++ Social-app/backend/server.js
@@ -44,7 +44,7 @@ app.use("/api/messages",messageRoutes)
 // http://localhost:5000 =>backend,frontend
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "frontend","dist")))
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     // react app
     app.get("*",(req,res) =>{
