2b95867d29466f2a8f3e694918672dd5406c6308
haunguyen1611 <150682278+haunguyen1611@users.noreply.github.com>
2024-12-26 20:14:01 +0700

    Update server.js

--- Social-app/backend/server.js
+++ Social-app/backend/server.js
@@ -44,11 +44,11 @@ app.use("/api/messages",messageRoutes)
 // http://localhost:5000 =>backend,frontend
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend","/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     // React app
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend","dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
     });
 }
 
